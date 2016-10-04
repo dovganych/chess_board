5c3d75892c91afce6624523e9e9db2ad823c02ef
dovganych <misha.dovganych@gmail.com>
2016-10-04 19:54:04 +0000

    Initial commit

--- script.js
+++ script.js
@@ -1,107 +1,109 @@
-var n,x,y,step; 
-var sqr;  
-
-var dx = [0,  2,  1, -1, -2, -2, -1, 1, 2]; 
-var dy = [0, -1, -2, -2,  1, -1,  2, 2, 1];
-var board = [] ;    
-
-
-function Move(x,y) {
-  var j, u, v, ways=[0,8],res,u1,v1;
-  //if((sqr % 2) != 0){sqr-=1;}
-  for(j = 1; j <= 8 ; j++)  
+var boardSize,x_initial,y_initial,STEP,allSquare; 
+var dx = [2,  1, -1, -2, -2, -1, 1, 2]; 
+var dy = [-1, -2, -2,  1, -1,  2, 2, 1];
+var board = [] ;  
+function doInitia() 
+{
+    STEP=1;
+    boardSize=10; 
+    allSquare = boardSize * boardSize;
+    x_initial = y_initial = 0;
+    for(var i = 0; i < boardSize; i++){ board [i] = []; }
+    board[x_initial][y_initial] = STEP;
+    STEP++;
+    Move(x_initial,y_initial);
+    drawBoard();
+} 
+function changeBoard() 
+{
+    STEP=1;
+    boardSize = parseInt(document.getElementById("boardSize").value);
+    x_initial = parseInt(document.getElementById("x_initial").value) - 1;
+    y_initial = parseInt(document.getElementById("y_initial").value) - 1;
+    allSquare = boardSize * boardSize;
+    for(var i = 0; i < boardSize; i++){ board [i] = []; }
+    board[x_initial][y_initial] = STEP;
+    STEP++;
+    Move(x_initial,y_initial);
+    drawBoard();
+} 
+function Move(intial_x,intial_y)
+{
+  var nextStep_X, nextStep_Y,countWays,lastStep_X,lastStep_Y;
+  var nextMove = 
+  {
+   minCountWays:8,
+   step_X:undefined,
+   step_Y:undefined,
+  };
+  for(var j = 0; j <= 8 ; j++)  
     {    
-        u = x + dx[j];     
-        v = y + dy[j];    
-        if ( 0 <= u && u < n && 0 <= v && v < n && (board[u][v] == 0))
+        nextStep_X = intial_x + dx[j];     
+        nextStep_Y = intial_y + dy[j];    
+        if ( 0 <= nextStep_X && nextStep_X < boardSize && 0 <= nextStep_Y && nextStep_Y < boardSize && (board[nextStep_X][nextStep_Y] == undefined))
         {   
-            res=find(u,v);
-            if( res != 0)
+            countWays=Find(nextStep_X,nextStep_Y);
+            if( countWays != 0 && countWays <= nextMove.minCountWays )
             {
-                ways[0] =res; 
-                if( ways[0] <= ways[1] )
-                    {
-                        ways[1] = ways[0];
-                        ways[2] = u;
-                        ways[3] = v;
-                    }
+                nextMove.minCountWays = countWays;
+                nextMove.step_X = nextStep_X;
+                nextMove.step_Y = nextStep_Y; 
             }
             else
             {
-                u1=u;
-                v1=v;
+                lastStep_X=nextStep_X;
+                lastStep_Y=nextStep_Y;
             }
         }
-    }	
-    if( ways[2] == undefined){ board[u1][v1] = step;return }
-    board[ways[2]][ways[3]] = step;
-    if (step < sqr)
-      {
-        step++;
-        Move (ways[2], ways[3]);
-      }       
-    else 
-        { return }
+    }  
+    if ( countWays == undefined) { return; } 
+    if ( nextMove.step_X == undefined) { board[lastStep_X][lastStep_Y] = STEP; return; }
+    board[nextMove.step_X][nextMove.step_Y] = STEP;
+    if (STEP < allSquare)
+        {
+            STEP++;
+            Move(nextMove.step_X, nextMove.step_Y);
+        }       
+    else { return }
 } 
-function find(x1,y1) {
-    var j, u, v, way=0;
-    if(sqr== step){ way=1; return way;}
-    for(j = 1; j <= 8 ; j++)  
+function Find(x,y) 
+{
+    var x1, y1, countWay=0;
+    if( allSquare == STEP){ return 1; }
+    for(var j = 0; j <= 8 ; j++)  
     {    
-        u = x1 + dx[j];     
-        v = y1 + dy[j];    
-        if ( 0 <= u && u < n && 0 <= v && v < n && (board[u][v] == 0))
-        {    
-            way++;
-        }  
+        x1 = x + dx[j];     
+        y1 = y + dy[j];    
+        if ( 0 <= x1 && x1 < boardSize && 0 <= y1 && y1 < boardSize && (board[x1][y1] == undefined)){ countWay++; }  
     }
-    return way;
+    return countWay;
 }
-
-function masCreate() {
-    step=1;
-    n=prompt('Enter size:',5); 
-    sqr = n * n;
-    var num1 =  prompt("Enter a Value","0");
-    var num2 = prompt("Enter a Value", "0");
-    x = parseInt(num1);
-    y = parseInt(num2);
-    for(var i = 0; i < n; i++)
+function drawBoard() 
+{
+    var x=0,y=0;
+    var canvas=document.getElementById("board");
+    canvas.style.border="1px black solid";
+    canvas.width=boardSize*50;
+    canvas.height=boardSize*50;
+    var draw= canvas.getContext('2d');
+    for(var i = 0; i < boardSize; i++)
     {
-        board [i] = [];
-        for(var j = 0; j < n; j++)
+        for(var j = 0; j < boardSize; j++)
         {
-            board [i][j] =0;
+            if ((i % 2 == 0 && j % 2 == 0) || (i % 2 != 0 && j % 2 != 0)){draw.fillStyle="black";}
+            else {draw.fillStyle="white";}
+            draw.fillRect(x,y,50,50);
+            draw.font = "20px";
+            draw.fillStyle = "red";
+            if(board[i][j] != undefined) { draw.fillText( board[i][j],x+20,y+30); }
+            x+=50;     
         }
-    }
-    board[x][y] = step;
-    step++;
-    masShow();
-}
-
-function masShow() {
-    $( ".white" ).remove();
-    $( ".break" ).remove();
-    $( ".black" ).remove();
-    var div;
-    for(var i = 0; i < n; i++)
-    {
-        div = document.createElement("div");
-        div.className += "break";
-        document.body.appendChild(div);
-        for(var j = 0; j < n; j++)
-        {
-
-            div = document.createElement("div");
-            if ((i % 2 == 0 && j % 2 == 0) || (i % 2 != 0 && j % 2 != 0)) {div.className = "black";}
-            else {div.className = "white";}
-            div.innerHTML=board[i][j];
-            document.body.appendChild(div);
-        }
-
+        y+=50;
+        x=0;
     }
 }
-function Run() {
-    Move(x,y);
-    masShow();
+function change(e)
+{
+    document.getElementById("x_initial").max= e.target.value;
+    document.getElementById("y_initial").max= e.target.value;
 }
